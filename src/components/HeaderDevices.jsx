e01834263280b8e3d0f44319537d8adc3c9af551
Zikama <nehemiezikama@gmail.com>
2021-10-17 13:25:17 +0300

    add the user circle icon for accounts

--- src/components/HeaderDevices.jsx
+++ src/components/HeaderDevices.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {Search} from 'react-feather';
-import {ReactComponent as User} from '../assets/images/user_driver.svg';
+import {ReactComponent as User} from '../assets/images/user_circle.svg';
 
 import Button from './Button.jsx';
 
